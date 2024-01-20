691ed379efab7e3a88266fe016d7a0a248033b2f
SanthiPriya <150931176+santhipriyakarthikeyan@users.noreply.github.com>
2024-01-20 18:11:44 +0530

    initial Commit

---
+++ src/main/java/net/javaguides/springboot/SpringBootFullstackApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackApplication.class, args);
+	}
+
+}
